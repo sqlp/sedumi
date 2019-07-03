sqlp/sedumi

commit 976bdd7b1c30ea774f7bf0c2894030daa78e035c

Lots of minor edits to suppress matlab code analyzer messages. Functional changes coming in separate commits. vec.m: rewrite to use numel() instead of m*n.

conversion/prelp.m
@@ -21,7 +21,7 @@
 %
 % See also sedumi, getproblem, postprocess (LIPSOL), frompack, lipsol.
 
-function [A,b,c,lenx,lbounds] = prelp(pname)
+function [A,b,c,lenx,lbounds,times] = prelp(pname)
 %
 % This file is part of SeDuMi 1.1 by Imre Polik and Oleksandr Romanko
 % Copyright (C) 2005 McMaster University, Hamilton, CANADA  (since 1.1)
@@ -55,7 +55,7 @@ function [A,b,c,lenx,lbounds] = prelp(pname)
 
 global OUTFID
 global Ubounds_exist
-
+if ~exist('loadata','file') || ~exist('preprocess','file')
 if ~exist('loadata','file') | ~exist('preprocess','file')
     error('To use PRELP, you need to have LIPSOL installed.')
 end
@@ -66,7 +66,7 @@ end
 if (nargin == 0)
     pname = input('Enter problem name: ','s');
 end
-
+[A,b,c,lbounds,ubounds,BIG] = loadata(pname);
 t0 = cputime;
 [A,b,c,lbounds,ubounds,BIG,NAME] = loadata(pname);
 times(1) = cputime - t0;

adendotd.m
@@ -1,4 +1,4 @@
-function Ad = Adendotd(dense, d, sparAd, Ablk, blkstart)
+function Ad = Adendotd(dense, d, sparAd, Ablk, blkstart) %#ok
 % Ad = Adendotd(dense, d, sparAd, Ablk, blkstart)
 %
 % ADENDOTD  Computes d[k]'*Aj[k] for Lorentz blocks that are to be factored

blkchol.m
@@ -1,4 +1,4 @@
-function [LL, Ld, Lskip, Ladd] = blkchol(L,X,pars,absd)
+function [LL, Ld, Lskip, Ladd] = blkchol(L,X,pars,absd) %#ok
 % [L.L, L.d, L.skip, L.add] = blkchol(L,X,pars,absd)
 %
 % BLKCHOL Fast block sparse Cholesky factorization.

bwblkslv.m
@@ -1,4 +1,4 @@
-function y = bwblkslv(L,b)
+function y = bwblkslv(L,b) %#ok
 % BWBLKSLV Solves block sparse upper-triangular system.
 %    y = bwblkslv(L,b) yields the same result as
 %              y(L.perm,:) = L.L'\b

bwdpr1.m
@@ -1,4 +1,4 @@
-function y = bwdpr1(Lden, b)
+function y = bwdpr1(Lden, b) %#ok
 % y = bwdpr1(Lden, b)
 %
 % BWDPR1  Solves "PROD_k L(pk,betak)' * y = b", where

checkpars.m
@@ -1,4 +1,4 @@
-function pars = checkpars(pars,lponly)
+function pars = checkpars(pars,lponly) %#ok
 % pars = checkpars(pars,lponly)
 %
 % CHECKPARS  Fills in defaults for missing fields in "pars" structure.

conversion/getproblem.m
@@ -91,7 +91,7 @@ end
 % Try to get problem in LIPSOL format
 % ----------------------------------------
 if(status == 0)
-    if(exist('findprob') > 1)
+    if(exist('findprob','file') > 1)
         if (findprob([],pname) == 1)
             status = 2;
         end

conversion/writesdp.m
@@ -50,7 +50,7 @@ end;
 %
 %  Check for any rotated cone constraints.
 %
-if (isfield(K,'r') & (~isempty(K.r)) & (K.r ~= 0)),
+if (isfield(K,'r') && (~isempty(K.r)) && (K.r ~= 0)),
   disp('rotated cone constraints are not yet supported.');
   return;
 end; 
@@ -134,7 +134,7 @@ else
   for i=1:nsdpblocks,
     fprintf(fid,'1\n');
     work=c(base:base+K.s(i)^2-1);
-    work=reshape(work,K.s(i),K.s(i));
+    if nnz(work ~= work'),
     if (work ~= work'),
       disp('Non symmetric C.s matrix!');
       work=(work+work')/2;
@@ -161,7 +161,7 @@ else
     base=sizelin+sizeq+1;
     for i=1:nsdpblocks,
       fprintf(fid,'1\n');
-      work=A(cn,base:base+K.s(i)^2-1);
+      if nnz(work ~= work'),
       work=reshape(work,K.s(i),K.s(i));
       if (work ~= work'),
         disp('Non symmetric A.s matrix!');

commit 8eb837f5ddb6805117269f042bb6f5a41836c182

prelp.m: Remove duplicate line.
Problem introduced with https://github.com/sqlp/sedumi/commit/976bdd7b1c30ea774f7bf0c2894030daa78e035c#diff-68d84bf6b656bd49b655a11aa59595aa .

conversion/prelp.m
@@ -56,7 +56,6 @@ function [A,b,c,lenx,lbounds,times] = prelp(pname)
 global OUTFID
 global Ubounds_exist
 if ~exist('loadata','file') || ~exist('preprocess','file')
-if ~exist('loadata','file') | ~exist('preprocess','file')
     error('To use PRELP, you need to have LIPSOL installed.')
 end
 