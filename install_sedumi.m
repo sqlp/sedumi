83df6f279e8691c7b22ce0a5f11432d305c2d79c
Michael Grant <mgrant@anaconda.com>
2024-04-10 13:18:10 -0500

    remove mex files, add buliding to CI

--- install_sedumi.m
+++ install_sedumi.m
@@ -111,24 +111,41 @@ targets64 = {...
 
 % Get first file of "targets64" and replace ".c" with mex-extension.
 % Keep order of "targets64".
+mext = mexext;
 mex_binaries = cellfun (@strtok, targets64, 'UniformOutput', false);
-mex_binaries = cellfun (@(x) strrep (x, '.c', ['.', mexext()]), ...
+mex_binaries = cellfun (@(x) strrep (x, '.c', ['.', mext]), ...
   mex_binaries, 'UniformOutput', false);
 
+fs = filesep();
 sedumi_path = mfilename ('fullpath');
-sedumi_path = sedumi_path(1:max ([1, strfind(sedumi_path, filesep ())]) - 1);
+sedumi_path = sedumi_path(1:max ([1, strfind(sedumi_path, fs)]) - 1);
 
+% Note the use of 0.01 here. That's because version 7 had more than 10
+% minor releases, so 7.10-7.14 need to be ordered after 7.01-7.09.
+VERSION  = [1, 0.01] * sscanf (version (), '%d.%d');
+COMPUTER = computer ();
 ISOCTAVE = (exist ('OCTAVE_VERSION', 'builtin') == 5);
+mdir = '';
 if (ISOCTAVE)
   prog = 'Octave';
   page_output_immediately (true, 'local');
+  switch COMPUTER
+  case 'x86_64-pc-linux-gnu'
+      mdir = 'o_lin';
+  case 'x86_64-apple-darwin21.6.0'
+      mdir = 'o_maci';
+  case 'aarch64-apple-darwin23.4.0'
+      mdir = 'o_maca';
+  case 'i686-w64-mingw32'
+      mdir = 'o_win';
+  case 'x86_64-w64-mingw32'
+      mdir = 'o_win';
+  otherwise
+    error(sprintf('Unexpected computer type: %s', COMPUTER))
+  end
 else
   prog = 'Matlab';
 end
-
-% Helper function to lookup existing mex binaries.
-count_mex_binaries = @() sum (cellfun (@(x) (exist (x, 'file') == 3), ...
-  fullfile (sedumi_path, mex_binaries)));
 
 line_sep = repmat ('-', 1, 75);
 fprintf ('\n%s\nSeDuMi installation script\n', line_sep);
@@ -141,11 +158,19 @@ disp (line_sep);
 % guarantee that rebuilding will work.
 if (~need_rebuild)
   fprintf ('Looking for existing binaries...');
-  nfound = count_mex_binaries ();
-  if (nfound == 0)
+  nfound = [0, 0];
+  for k = 1 : length(targets64)
+      targ = mex_binaries{k};
+      if exist( [ sedumi_path, fs, targ ], 'file' )
+          nfound(1) = nfound(1) + 1;
+      elseif ~isempty(mdir) && exist( [ sedumi_path, fs, mdir, fs, targ ], 'file' )
+          nfound(2) = nfound(2) + 1;
+      end
+  end
+  if sum(nfound) == 0
     fprintf ('none found; building...\n');
     need_rebuild = true;
-  elseif (nfound < length(targets64))
+  elseif sum(nfound) < length(targets64)
     fprintf ('incomplete set found.\n');
     disp (line_sep);
     error (['%s\n', ...
@@ -163,95 +188,121 @@ if (~need_rebuild)
 end
 
 if (need_rebuild)
+  mdir = '';
   disp ('Attempting to recompile the SeDuMi binaries:');
 
   % Customization by providing a mex template.
-  if (isempty (mex_template))
-    flags = {};
-    libs = {};
-
-    if (ISOCTAVE)
-      % Matlab mex optimization '-O' corresponds to gcc '-O2',
-      % whereas calling '-O' would result in gcc '-O1'.
-      flags{end+1} = '-O2';
-      flags{end+1} = '-DOCTAVE';
-      flags{end+1} = '-Wall';
-      if (ismac ())
-        % Assume Homebrew (https://brew.sh/) installation.
-        % https://stackoverflow.com/questions/50634727/dyld-library-not-loaded-usr-local-opt-openblas-lib-libopenblasp-r0-2-20-dylib
-        flags{end+1} = '-I/usr/local/opt/openblas/include';
-        libs{end+1}  = '-L/usr/local/opt/openblas/lib -lopenblas';
-      elseif (ispc ())
-        libs{end+1}  = '-lopenblas';
+  flags = {};
+  libs = {};
+  if ~isempty(mex_template)
+    found = false;
+    [cmd, remain] = strtok(mex_template);
+    while ~isempty(remain)
+      [tok, remain] = strtok(remain);
+      if found
+        libs{end+1} = tok;
+      elseif tok == '%s'
+        found = true;
       else
-        % Including the default OpenBLAS path works for most Octave
-        % installations and does not harm if not present.
-        flags{end+1} = '-I/usr/include/openblas';
-      end
-    else % Matlab
-      % Note the use of 0.01 here. That's because version 7 had more than 10
-      % minor releases, so 7.10-7.14 need to be ordered after 7.01-7.09.
-      VERSION  = [1, 0.01] * sscanf (version (), '%d.%d');
-      COMPUTER = computer ();
-      % The last Matlab release with a 32 bit version was R2015b.
-      IS64BIT  = strcmp (COMPUTER(end-1:end), '64');
-      flags{end+1} = '-O';  % optimize
-      if (IS64BIT && (VERSION >= 7.03)) % R2006b
-        flags{end+1} = '-largeArrayDims';
-      elseif (VERSION < 7.03)           % R2006b
-        flags{end+1} = '-DmwIndex=int';
-        flags{end+1} = '-DmwSize=int';
-        flags{end+1} = '-DmwSignedIndex=int';
-        flags{end+1} = '-DFWRAPPER';
-      end
-      if ((VERSION >= 7) && ispc ())  % R14 (2004)
-        if IS64BIT
-          dirval = 'win64';
-        else
-          dirval = 'win32';
-        end
-        libdir = fullfile (matlabroot (), 'extern', 'lib', dirval);
-        if exist (fullfile (libdir, 'microsoft'), 'dir')
-          libdir = fullfile (libdir, 'microsoft');
-          found = true;
-        elseif exist (fullfile (libdir, 'msvc60'), 'dir')
-          libdir = fullfile (libdir, 'msvc60');
-          found = true;
-        elseif exist (fullfile (libdir, 'lcc'), 'dir')
-          libdir = fullfile (libdir, 'lcc');
-          found = true;
-        end
-        if found
-          libs{end+1} = ['-L''', strrep(libdir, '\', '\\'), ''''];
-        end
-      end
-      if (VERSION >= 7.05)  % R2007b
-        libs{end+1} = '-lmwblas';
-      else
-        libs{end+1} = '-lmwlapack';
+        flags{end+1} = tok;
       end
     end
-    mex_template = ['mex', sprintf(' %s', flags{:}), ' %s', ...
-      sprintf(' %s', libs{:})];
+  elseif (ISOCTAVE)
+    % Matlab mex optimization '-O' corresponds to gcc '-O2',
+    % whereas calling '-O' would result in gcc '-O1'.
+    cmd = 'mkoctfile';
+    flags{end+1} = '--mex';
+    flags{end+1} = '-O2';
+    flags{end+1} = '-DOCTAVE';
+    flags{end+1} = '-Wall';
+    if (ismac ())
+      % Assume Homebrew (https://brew.sh/) installation.
+      % https://stackoverflow.com/questions/50634727/dyld-library-not-loaded-usr-local-opt-openblas-lib-libopenblasp-r0-2-20-dylib
+      % this glob covers /opt/homebrew and /usr/local installations
+      homebrew = glob('/*/*/Cellar/openblas/*/include');
+      flags{end+1} = ['-I', homebrew{1}];
+      libs{end+1}  = ['-L', strrep(homebrew{1}, '/include', '/lib'), ' -lopenblas'];
+    elseif (ispc ())
+      libs{end+1}  = '-lopenblas';
+    else
+      % Including the default OpenBLAS path works for most Octave
+      % installations and does not harm if not present.
+      flags{end+1} = '-I/usr/include/openblas';
+    end
+  else % Matlab
+    cmd = 'mex';
+    % The last Matlab release with a 32 bit version was R2015b.
+    IS64BIT  = strcmp (COMPUTER(end-1:end), '64');
+    flags{end+1} = '-O';  % optimize
+    if (IS64BIT && (VERSION >= 7.03)) % R2006b
+      flags{end+1} = '-largeArrayDims';
+    elseif (VERSION < 7.03)           % R2006b
+      flags{end+1} = '-DmwIndex=int';
+      flags{end+1} = '-DmwSize=int';
+      flags{end+1} = '-DmwSignedIndex=int';
+      flags{end+1} = '-DFWRAPPER';
+    end
+    if ((VERSION >= 7) && ispc ())  % R14 (2004)
+      if IS64BIT
+        dirval = 'win64';
+      else
+        dirval = 'win32';
+      end
+      libdir = fullfile (matlabroot (), 'extern', 'lib', dirval);
+      if exist (fullfile (libdir, 'microsoft'), 'dir')
+        libdir = fullfile (libdir, 'microsoft');
+        found = true;
+      elseif exist (fullfile (libdir, 'msvc60'), 'dir')
+        libdir = fullfile (libdir, 'msvc60');
+        found = true;
+      elseif exist (fullfile (libdir, 'lcc'), 'dir')
+        libdir = fullfile (libdir, 'lcc');
+        found = true;
+      end
+      if found
+        libs{end+1} = ['-L''', strrep(libdir, '\', '\\'), ''''];
+      end
+    end
+    if (VERSION >= 7.05)  % R2007b
+      libs{end+1} = '-lmwblas';
+    else
+      libs{end+1} = '-lmwlapack';
+    end
   end
 
-  fprintf ('Template: %s\n', mex_template);
+  mex_template = sprintf(' %s', cmd, flags{:}, '%s', libs{:});
+  fprintf ('Template:%s\n', mex_template);
 
   % Move to SeDuMi root directory and compile.
+  failed = false;
   olddir = cd (sedumi_path);
   for i = 1:length(targets64)
-    fprintf ('   %-16s%s\n', [mex_binaries{i}, ':'], targets64{i});
-    eval (sprintf (mex_template, targets64{i}));
+    files = {};
+    remain = targets64{i};
+    while ~isempty(remain),
+      [fname, remain] = strtok(remain);
+      files{end+1} = fname;
+    end
+    fprintf('  %s: %s\n', mex_binaries{i}, targets64{i});
+    try
+      if ISOCTAVE
+        mkoctfile(flags{:}, files{:}, libs{:});
+      else
+        mex(flags{:}, files{:}, libs{:})
+      end
+    catch
+      failed = true;
+    end
   end
   cd (olddir);
-end
-
-% Final check if all mex binaries are present.
-if (count_mex_binaries () < length (targets64))
-  error (['%s\nSeDuMi was not successfully installed.\n', ...
-    'Please attempt to correct the errors and try again.\n\n', ...
-    'For example, try setting the compilation command:\n\n', ...
-    '  install_sedumi -rebuild ''%s'''], line_sep, mex_template);
+  if failed
+    error (['%s\nSeDuMi was not successfully installed.\n', ...
+      'Please attempt to correct the errors and try again.\n\n', ...
+      'For example, try setting the compilation command:\n\n', ...
+      '  install_sedumi -rebuild ''%s'''], line_sep, mex_template);
+  end
+elseif (~isempty(mdir) && ~exist(fullfile(sedumi_path, mdir)))
+  mdir = '';
 end
 
 % Add SeDuMi to search path, if not forbidden by "-nopath".
@@ -273,6 +324,18 @@ if (~no_path)
   else
     fprintf ('already there.\n');
   end
+  if (~isempty(mdir))
+    fprintf ('   MEX directory... ');
+    spath = fullfile(sedumi_path, mdir);
+    if (~exist(spath, 'dir'))
+      fprintf('not present.\n');
+    elseif (~is_on_search_path (spath))
+      addpath (spath);
+      fprintf ('added.\n');
+    else
+      fprintf ('already there.\n');
+    end
+  end
   fprintf ('   Conversion directory... ');
   if (~is_on_search_path (fullfile (sedumi_path, 'conversion')))
     addpath (fullfile (sedumi_path, 'conversion'));
@@ -293,5 +356,5 @@ disp (line_sep);
 disp ('SeDuMi has been successfully installed.');
 disp ('For more information, type "help sedumi" or see the user guide.');
 disp (line_sep);
-fprintf ('\n\n');
+disp ('');
 end
